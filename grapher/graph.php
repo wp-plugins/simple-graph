80c6de67b0b619a5758820832ccfe367b312a730
pacius <pacius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-04-03 14:16:52 +0000

    git-svn-id: https://plugins.svn.wordpress.org/simple-graph/trunk@10127 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ grapher/graph.php
@@ -0,0 +1,249 @@
+<?php
+// check capabilities
+$imagecreate = "imagecreatetruecolor";
+$imagecolor = "imagecolorexact";
+if (!function_exists($imagecreate)) {
+    $imagecreate = "imagecreate";
+	$imagecolor = "imagecolorallocate";
+}
+$imagemime = "image/png";
+$imageout = "imagepng";
+if (!function_exists($imageout)) {
+	$imagemime = "image/gif";
+	$imageout = "imagegif";
+}
+if (!function_exists($imageout)) {
+	$imagemime = "image/jpeg";
+	$imageout = "imagejpeg";
+}
+// get size and params
+$gwidth = 0; $gheight = 0;
+if (isset($_GET['w'])) $gwidth = $_GET['w'];
+if (isset($_GET['h'])) $gheight = $_GET['h'];
+$show_trend = FALSE;
+if (isset($_GET['t'])&&$_GET['t']=='1') $show_trend = TRUE;
+$start_date = FALSE;
+if (isset($_GET['ytd'])&&$_GET['ytd']=='1') 
+	$start_date = mktime(date("H"),date("i"),date("s"),date("m"),date("d"),date("Y")-1);
+else if (isset($_GET['lm'])&&$_GET['lm']=='1')
+	$start_date = mktime(date("H"),date("i"),date("s"),date("m")-1,date("d"),date("Y"));
+$weekly = FALSE;
+if (isset($_GET['wkly'])&&$_GET['wkly']=='1') $weekly = TRUE;
+// error function
+function error_msg($str1,$str2) {
+	global $imagecreate, $imagecolor;
+	$img = $imagecreate(350,40);
+	$bgcol = $imagecolor($img,0,0,0);
+	$fgcol = $imagecolor($img,255,255,255);
+	imagefill($img,1,1,$bgcol);
+	imagestring($img,3,4,2,$str1,$fgcol);
+	imagestring($img,2,4,12,$str2,$fgcol);
+	header("Content-Type: image/gif");
+	imagegif($img);
+	exit();
+}
+// load wordpress config
+if (!file_exists( dirname(__FILE__) . '/../../../../wp-config.php' )) {
+	error_msg("Graph Plugin is not properly installed.","wp-config.php not found. Check installation path.");
+}
+require_once ( dirname(__FILE__) . '/../../../../wp-config.php' );
+// dirty hack to get wp 2.1 compatibility :)
+$table_prefix = $wpdb->prefix;
+// get date format
+$datefmt = "y/m/d";
+if (get_option('pjm_graph_datefmt'))
+	$datefmt = get_option('pjm_graph_datefmt');
+
+// helper functions
+function getColor($hex,$image) {
+	global $imagecolor;
+	$r = hexdec(substr($hex,0,2));
+	$g = hexdec(substr($hex,2,2));
+	$b = hexdec(substr($hex,4,2));
+	$c = $imagecolor($image,$r,$g,$b);
+	return $c;
+}
+function parseConf() {
+	global $gwidth, $gheight, $imagecreate, $imagecolor;
+	$width = get_option('pjm_graph_width');
+	$height = get_option('pjm_graph_height'); 
+	if ($gwidth!=0) $width = $gwidth;
+	if ($gheight!=0) $height = $gheight;
+	$image = $imagecreate($width,$height);
+	$background = getColor(get_option('pjm_graph_bgcolor'),$image); 
+	$foreground = getColor(get_option('pjm_graph_fgcolor'),$image); 
+	$linecol = getColor(get_option('pjm_graph_linecolor'),$image); 
+	$bglinecol = getColor(get_option('pjm_graph_bglinecolor'),$image);
+	$trendcol = $bglinecol;
+	if (get_option('pjm_graph_trendcolor'))
+		$trendcol = getColor(get_option('pjm_graph_trendcolor'),$image);
+	return array($image,$width,$height,$background,$foreground,$linecol,$bglinecol,$trendcol);
+}
+function parseData() {
+	global $wpdb, $table_prefix, $start_date;
+	$sql = "SELECT MAX(stamp) AS highdate, MIN(stamp) AS lowdate, "
+	     . "MAX(value) AS highvalue, MIN(value) AS lowvalue "
+	     . "FROM ".$table_prefix."pjm_graph";
+	if ($start_date !== FALSE)
+		$sql .= " WHERE stamp > $start_date";
+	if ( $valueset = $wpdb->get_results($sql) ) {
+		foreach ($valueset as $values) {
+			$highvalue = $values->highvalue;
+			$lowvalue = $values->lowvalue;
+			$highdate = $values->highdate;
+			$lowdate = $values->lowdate;
+			return array($highvalue,$lowvalue,$highdate,$lowdate);		
+		}
+	}	
+}
+
+// execute
+list($image,$width,$height,$bgcol,$fgcol,$linecol,$bglinecol,$trendcol) = parseConf();
+// background color
+imagefill($image,1,1,$bgcol);
+// parse data
+list($highvalue,$lowvalue,$highdate,$lowdate) = parseData();
+//print("$highvalue $lowvalue $highdate $lowdate"); flush();
+$graphhigh = $highvalue * 1.03;
+$graphlow  = $lowvalue  * 0.97;
+$midvalue   = ($graphhigh+$graphlow)/2.0;
+$leftmargin = strlen(" ".(int)$graphhigh)*5;
+if ($leftmargin < 20) $leftmargin = 20;
+$rightmargin = 2;
+$margins = $leftmargin + $rightmargin;
+$graphwidth = $width - $margins;
+$days = (int)(($highdate - $lowdate)/60/60/24);
+if ($weekly)
+	$days = $days / 7;
+$daygap = 1; $gappixels = 1;
+if (($graphwidth)/$days>3) {
+	$daygap = 1;
+	$gappixels = (($graphwidth)/$days);
+} else if (($graphwidth)/$days*7>3) {
+	$daygap = 7;
+	$gappixels = (($graphwidth)/$days*7);
+} else if (($graphwidth)/$days*30>3) {
+	$daygap = 30;
+	$gappixels = (($graphwidth)/$days*30);
+} else if (($graphwidth)/$days*100>3) {
+	$daygap = 100;
+	$gappixels = (($graphwidth-22)/$days*100);
+} else if (($graphwidth)/$days*365>3) {
+	$daygap = 365;
+	$gappixels = (($graphwidth)/$days*365);
+} else {
+	$daygap = 1000;
+	$gappixels = (($graphwidth)/$days*1000);
+}
+// graph layout
+imagestring($image,1,2,1,sprintf("% 3d",$graphhigh),$fgcol);
+imagestring($image,1,2,2+(($height-2-7)/2)-4,sprintf("% 3d",$midvalue),$fgcol);
+imagestring($image,1,2,$height-2-6-7,sprintf("% 3d",$graphlow),$fgcol);
+imageline($image,$leftmargin-1,2,$width-$rightmargin,2,$bglinecol);
+imageline($image,$leftmargin-1,2,$leftmargin,2,$fgcol);
+imageline($image,$leftmargin-1,2+(($height-2-7)/2),$width-$rightmargin,2+(($height-2-7)/2),$bglinecol);
+imageline($image,$leftmargin-1,2+(($height-2-7)/2),$leftmargin+1,2+(($height-2-7)/2),$fgcol);
+imageline($image,$leftmargin-1,$height-2-7,$width-$rightmargin,$height-2-7,$fgcol);
+imageline($image,$leftmargin-2,2,$leftmargin-2,$height-2-7,$fgcol);
+$x = $leftmargin + $gappixels; $date = $lowdate; $lastdate = 0;
+while ($x<$width-2) {
+	imageline($image,$x,$height-3-7,$x,$height-4-7,$fgcol);
+	$div = 1;
+	if ($weekly) $div = 7;
+	$date += $daygap*24*60*60 * $div;
+	$datelen = strlen(date($datefmt,$date)) * 5 + 4;
+	if ($lastdate<$x-$datelen) {
+		$tx = $x-20;
+		if ($tx>$width-42) $tx = $width-41;
+		if ($lastdate<$tx-($datelen/2)) {
+			//imagestring($image,1,$tx,$height-7,sprintf("%02d.%02d.%02d",date("d",$date),date("m",$date),date("y",$date)),$fgcol);
+			imagestring($image,1,$tx,$height-7,date($datefmt,$date),$fgcol);
+			imageline($image,$x,$height-12,$x,2,$bglinecol);
+			$lastdate = $x;
+		}
+	}
+	$x += $gappixels; 
+} 
+// plot data 
+$lowpoint = $height-2-7;
+$highpoint = 2; $lastdate = $lowdate; $prevdate = $lowdate;
+$lastx = FALSE; $lasty = FALSE; $last_trend_y = FALSE;
+$onepoint = ($lowpoint-$highpoint)/($graphhigh-$graphlow);
+$oneday = ($width-$margins)/$days; $trendpoint = FALSE;
+$lowpoint_shown = FALSE; $highpoint_shown = FALSE;
+$date_clause = "";
+if ($start_date!==FALSE)
+	$date_clause = " WHERE stamp > $start_date ";
+$sql = "SELECT * FROM ".$table_prefix."pjm_graph $date_clause ORDER BY stamp ASC";
+if ( $datalines = $wpdb->get_results($sql) ) {
+    if (count($datalines)<2)
+	error_msg("Not enough data to generate graph.","At least two data points must be inserted.");
+    $totalpoints = 0.0; $pointcount = 0.0;
+    foreach ($datalines as $dataline) {
+	$date = $dataline->stamp;
+	$dayssincelow = ($date-$lastdate)/60/60/24;
+	$dayssinceprev = ($date-$prevdate)/60/60/24;
+	$continue_point = true;
+	$todays_point = $dataline->value;
+	$div = 1;
+	if ($weekly) {
+		$div = 7;
+		$totalpoints += $dataline->value;
+		$pointcount++;
+		if ($dayssinceprev<7) {
+			$continue_point = false;
+		} else {
+			if ($pointcount==0) $pointcount = 1;
+			$todays_point = 1.0 * $totalpoints / $pointcount;
+			$totalpoints = 0;
+			$pointcount = 0;
+		}
+	}
+	if ($continue_point) {
+	$x = $leftmargin + $dayssincelow / $div * $oneday;
+	$y = $highpoint + ($graphhigh-$todays_point) * $onepoint;
+	if ($show_trend) {
+	if ($trendpoint === FALSE) {
+		$trendpoint = $todays_point;
+		$last_trend_y = $y;
+	} else {
+		$old_trendpoint = $trendpoint;
+		$trendpoint = round( $todays_point - $old_trendpoint ) / 10 + $old_trendpoint;
+		$trend_y = $highpoint + ($graphhigh - $trendpoint) * $onepoint;
+		imageline($image,$lastx,$last_trend_y,$x,$trend_y,$trendcol);
+		imageline($image,$x,$trend_y+1, $x, $trend_y - 1, $trendcol);
+		/*imageline($image,$x-1,$trend_y-1,$x+1,$trend_y+1,$bglinecol);
+		imageline($image,$x+1,$trend_y-1,$x-1,$trend_y+1,$bglinecol);
+		imageline($image,$x-2,$trend_y  ,$x+2,$trend_y  ,$bglinecol);
+		imageline($image,$x  ,$trend_y-2,$x  ,$trend_y+2,$bglinecol);*/
+		$last_trend_y = $trend_y;
+	}
+	}
+	if ($lastx!==FALSE) {
+		imageline($image,$lastx,$lasty,$x,$y,$linecol);
+	}
+	$lastx = $x;
+	$lasty = $y;
+	imageline($image,$x-1,$y-1,$x+1,$y+1,$linecol);
+	imageline($image,$x+1,$y-1,$x-1,$y+1,$linecol);
+	imageline($image,$x-2,$y  ,$x+2,$y  ,$fgcol  );
+	imageline($image,$x  ,$y-2,$x  ,$y+2,$fgcol  );
+	$tx = $x - 12.5;
+	if ($tx>($width-2-25)) $tx = $width - 2 - 25;
+	if ($tx<22) $tx = 22;
+	if (!$lowvalue_shown&&$todays_point==$lowvalue) {
+		imagestring($image,1,$tx,$y+3,sprintf("% 5.1f",$lowvalue),$fgcol);
+		$lowvalue_shown = TRUE;
+	}
+	if (!$highvalue_shown&&$todays_point==$highvalue) {
+		imagestring($image,1,$tx,$y-10,sprintf("% 5.1f",$highvalue),$fgcol);
+		$highvalue_shown = TRUE;
+	}
+	$prevdate = $date;
+	}
+    }
+}
+
+header("Content-Type: ".$imagemime);
+$imageout($image);
+?>
---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== Plugin Name ===
+Contributors: pacius
+Donate link: http://www.pasi.fi/simple-graph-wordpress-plugin/
+Tags: graph, chart, weight loss
+Requires at least: 2.0.1
+Tested up to: 2.0.4
+Stable tag: 0.9.6
+
+Draws a line graph of single set of date related data. Graph can be made public (i.e. sidebar) and the data can be manipulated through dashboard.
+
+== Description ==
+
+Draws a graph of single set of date related data. Graph can be made public (i.e. sidebar) and the data can be manipulated through dashboard. Many people use this plugin for weight tracking on their blogs (as do I), but it has been used for many other things as well. Only requirement - or rather a constraint - is that the horizontal axis of the graph shows dates. The vertical axis can hold any values.
+
+== Installation ==
+
+Requires that your PHP installation has GD support enabled. The plugin's configuration page shows information on this.
+
+First of all, backup your Wordpress files and database! Installation of this plugin shouldn't do any harm, but since I've done absolutely no testing besides using it on my WP 2.0.3 platform, I cannot guarantee it's entirely bug free. Also, this is my first ever WP plugin, which I created both for fun and to learn the art of making plugins. So, this plugin is provided AS IS, and installation and usage of this plugin is entirely at your own risk. I will not assume any responsibility for any possible damages. (Although it still isn't supposed to cause any damages.)
+
+Extract either one of the packages above in your Wordpress plugins folder (wp-content/plugins/) and then activate the plugin through your Wordpress administration. The activation sets up initial configuration options and creates the database table ($table_prefix.'pjm_graph') for the plugin data.
+
+Please make sure that you upload/extract the pjm_graph folder entirely in your plugin folder, and not just the contents of it! Your folder structure should look like this:
+
+    * wp-content/plugins/
+          o pjm_graph/
+                + pjm_graph.php
+                + grapher/
+                      # graph.php
+
+Finally, insert a code similar to the one below to your Wordpress theme, for example in the sidebar.php file. The essential part is calling the pjm_graph() function.
+
+`<?php if (function_exists('pjm_graph')) { ?>
+<li><h2>The Project</h2>
+<?php pjm_graph(); ?><br />
+<a href="http://www.pasi.fi/simple-graph-wordpress-plugin/">About this graph plugin…</a>
+</li>
+<?php } ?>`
+
+Installation is now complete!
+
+There are some parameters to that function though, so if you want to make your graph look different, you might want to check these out.
+
+You can override width and height of the graph with function parameters. Also, as of v0.9.3 it is possible to add optional trend graph with a boolean flag. In version 0.9.6 further parameters (target, ytd, lm, and wkly) were added. See the function declaration below.
+
+void pjm_graph($WIDTH, $HEIGHT, $TREND, $TARGET, $YTD, $MTD, $WKLY);
+
+Zero (0) values for WIDTH and HEIGHT preserve the default width and height that are specified in the admin panel. Any other values override the default.
+
+TREND, TARGET, YTD, MTD and WKLY parameters expect a boolean value, which is either TRUE or FALSE. By default they're all FALSE. If TREND is TRUE, gliding trend graph appears. If YTD is TRUE, only the values from last year are used in the graph. If MTD is TRUE, only the values from last month are used. (If YTD is TRUE, value of MTD is irrelevant.) If WKLY is TRUE, rough weekly average values will be calculated instead of daily values where possible. TARGET is not fully implemented yet and thus its value has no meaning at the moment.
+
+== Frequently Asked Questions ==
+
+= How about multiple graphs? =
+
+If a 1.0 release is ever finished, it will support multiple graphs. However, don't hold your breath.
+
+== Screenshots ==
+
+1. Data management page
+2. Plugin configuration page
+
